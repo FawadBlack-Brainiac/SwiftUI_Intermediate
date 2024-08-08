c167e3dba7f375f708efbb350e604d442949cf7f
fawadblack <foryoufawad@gmail.com>
2024-08-08 22:51:56 +0500

    Initial Commit

---
+++ SwiftUI_Intermediate/SwiftUI_IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntermediateApp.swift
+//  SwiftUI_Intermediate
+//
+//  Created by Qasim Ashraf on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
